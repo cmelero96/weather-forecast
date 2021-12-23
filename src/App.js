f517eeb36059e43552596edf6e61f30640dbc30d
carlos.melero <cmelero96@gmail.com>
2021-12-23 14:11:24 +0100

    Added TODO

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const languages = {
 };
 
 function App() {
+  /* TODO list of features:
+    - Search for a city from a dropdown list and get weather for that city
+    - Restrict country if searchable dropdown is too slow
+    - Create decent styling (probably using Tailwind)
+    - Make extra data hideable by clicking somewhere
+    - Remember past-searched cities
+    - Put past-searched cities somewhere visible, but collapsed so they don't bother
+    - TBD...
+  */
   return <WeatherPage city={city} language={languages.english}></WeatherPage>;
 }
 
