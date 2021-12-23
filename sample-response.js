c181e0da5b82d862fa511a38e7f5f70c8cd1b5eb
carlos.melero <cmelero96@gmail.com>
2021-12-23 11:17:52 +0100

    Added JSON with city list and sample hardcoded response from API.

---
+++ sample-response.js
@@ -0,0 +1,46 @@
+const sample = {
+  coord: {
+    lon: -3.7026,
+    lat: 40.4165,
+  },
+  weather: [
+    {
+      id: 801,
+      main: 'Clouds',
+      description: 'few clouds',
+      icon: '02d',
+    },
+  ],
+  base: 'stations',
+  main: {
+    temp: 282.12,
+    feels_like: 282.12,
+    temp_min: 279.93,
+    temp_max: 284.94,
+    pressure: 1021,
+    humidity: 89,
+  },
+  visibility: 8000,
+  wind: {
+    speed: 0.89,
+    deg: 135,
+    gust: 1.79,
+  },
+  clouds: {
+    all: 20,
+  },
+  dt: 1640254215,
+  sys: {
+    type: 2,
+    id: 2007545,
+    country: 'ES',
+    sunrise: 1640244925,
+    sunset: 1640278341,
+  },
+  timezone: 3600,
+  id: 3117735,
+  name: 'Madrid',
+  cod: 200,
+};
+
+export default sample;
